fae81a505cbd41ee78411b043e1c78c30e5c0269
ebaquera <87329972+ebaquera@users.noreply.github.com>
2023-09-22 11:20:45 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
-streamlit.title('Hello World')
+streamlit.title('My Parents New Healthy Diner')
+
+steramlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Oatmeal')
+streamlit.text('Eggs with spinach')
+streamlit.text('Hard-Boiled Free-Range Egg')
